7e9c25e5582f9ce18fbeb72cd050bbb557757120
Noel Vock <noel1uk@gmail.com>
2019-04-11 23:14:49 +0100

    behavior subject in action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BehaviorSubjectComponent } from './behavior-subject/behavior-subject.component';
+import { DataService } from './data-service/data.service';
+import { ParentComponent } from './parent/parent.component';
+import { SiblingComponent } from './sibling/sibling.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BehaviorSubjectComponent,
+    ParentComponent,
+    SiblingComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sibling/sibling.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data-service/data.service';
+
+@Component({
+  selector: 'app-sibling',
+  templateUrl: './sibling.component.html',
+  styleUrls: ['./sibling.component.scss']
+})
+export class SiblingComponent implements OnInit {
+
+  message: string;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.currentMessage$.subscribe(message => this.message = message);
+  }
+
+  newMessage() {
+    this.data.changeMessage('hello from sibling');
+  }
+
+}
